5b8ba3089dc166fe253b433beba4e895bf2a8223
Higor <higorpiresoliveira@gmail.com>
2022-05-16 17:44:41 -0300

    refactoring the code

--- test/data/usecases/load_topics_test.dart
+++ test/data/usecases/load_topics_test.dart
@@ -20,13 +20,64 @@ class HttpLoadTopics {
 
 @GenerateMocks([HttpClient])
 void main() {
+  late HttpLoadTopics sut;
+  late MockHttpClient httpClient;
+  late String url;
+  late List<Map> list;
+
+  List<Map> mockValidData() => [
+        {
+          'id': faker.guid.guid(),
+          'owner_id': faker.randomGenerator.string(50),
+          'parent_id': null,
+          'slug': faker.randomGenerator.string(20),
+          'title': faker.randomGenerator.string(20),
+          'body': faker.randomGenerator.string(250),
+          'status': 'published',
+          'source_url': null,
+          'created_at': faker.date.dateTime().toIso8601String(),
+          'updated_at': faker.date.dateTime().toIso8601String(),
+          'published_at': faker.date.dateTime().toIso8601String(),
+          'username': faker.randomGenerator.string(10),
+          'parent_title': null,
+          'parent_slug': null,
+          'parent_username': null,
+        },
+        {
+          'id': faker.guid.guid(),
+          'owner_id': faker.randomGenerator.string(50),
+          'parent_id': null,
+          'slug': faker.randomGenerator.string(20),
+          'title': faker.randomGenerator.string(20),
+          'body': faker.randomGenerator.string(250),
+          'status': 'published',
+          'source_url': null,
+          'created_at': faker.date.dateTime().toIso8601String(),
+          'updated_at': faker.date.dateTime().toIso8601String(),
+          'published_at': faker.date.dateTime().toIso8601String(),
+          'username': faker.randomGenerator.string(10),
+          'parent_title': null,
+          'parent_slug': null,
+          'parent_username': null,
+        },
+      ];
+
+  PostExpectation mockRequest() => when(httpClient.request(url: anyNamed('url'), method: anyNamed('method')));
+
+  void mockHttpData(List<Map> data) {
+    list = data;
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  setUp(() {
+    url = faker.internet.httpUrl();
+    httpClient = MockHttpClient();
+    sut = HttpLoadTopics(url: url, httpClient: httpClient);
+
+    mockHttpData(mockValidData());
+  });
+
   test('Should call HttpClient with correct values', () async {
-    final String url = faker.internet.httpUrl();
-    final httpClient = MockHttpClient();
-    final sut = HttpLoadTopics(url: url, httpClient: httpClient);
-
-    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'))).thenAnswer((_) async => Future.value(null));
-
     sut.loadAllTopics();
 
     verify(httpClient.request(url: url, method: 'get')).called(1);
