c8cd0f8b131d8ed1401621f925308db2464dc863
Higor <higorpiresoliveira@gmail.com>
2022-05-16 20:08:37 -0300

    ensure LoadTopics throws UnexpectedError if HttpClient returns 500

--- test/data/usecases/load_topics_test.dart
+++ test/data/usecases/load_topics_test.dart
@@ -154,4 +154,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.loadAllTopics();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
