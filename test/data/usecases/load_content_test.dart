e6139ddb5486a654461ed9c5d7a73d0949972822
Higor <higorpiresoliveira@gmail.com>
2022-05-16 20:28:31 -0300

    ensure HttpLoadContent throws UnexpectedError if HttpClient returns 500

--- test/data/usecases/load_content_test.dart
+++ test/data/usecases/load_content_test.dart
@@ -114,4 +114,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.loadContent(faker.guid.guid());
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
