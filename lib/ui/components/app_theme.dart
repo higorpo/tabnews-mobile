5eac74a10aae757b08aa637e5527daa36e39c0fc
Higor <higorpiresoliveira@gmail.com>
2022-05-17 11:34:14 -0300

    creating feed page structure

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -1,43 +1,20 @@
 import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
-  const primaryColor = Color.fromRGBO(136, 14, 79, 1);
-  const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
-  const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
-  const secondaryColorDark = Color.fromRGBO(0, 37, 26, 1);
+  const primaryColor = Color.fromRGBO(36, 41, 47, 1);
 
   return ThemeData(
     primaryColor: primaryColor,
-    primaryColorDark: primaryColorDark,
-    primaryColorLight: primaryColorLight,
-    accentColor: primaryColor,
-    backgroundColor: Colors.white,
-    secondaryHeaderColor: secondaryColorDark,
-    textTheme: TextTheme(
+    textTheme: const TextTheme(
       headline1: TextStyle(
         fontSize: 30,
         fontWeight: FontWeight.bold,
-        color: primaryColorDark,
+        color: primaryColor,
       ),
     ),
-    inputDecorationTheme: InputDecorationTheme(
-      enabledBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: primaryColorLight),
-      ),
-      focusedBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: primaryColor),
-      ),
-      alignLabelWithHint: true,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: primaryColor,
     ),
-    buttonTheme: ButtonThemeData(
-      colorScheme: ColorScheme.light(primary: primaryColor),
-      buttonColor: primaryColor,
-      splashColor: primaryColorLight,
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      textTheme: ButtonTextTheme.primary,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-    ),
+    scaffoldBackgroundColor: const Color.fromRGBO(238, 238, 238, 1),
   );
 }
--- lib/ui/pages/feed/feed_page.dart
+++ lib/ui/pages/feed/feed_page.dart
@@ -11,7 +11,7 @@ class FeedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Feed'),
+        title: Image.asset('lib/ui/assets/logo.png'),
       ),
       body: Container(),
     );
