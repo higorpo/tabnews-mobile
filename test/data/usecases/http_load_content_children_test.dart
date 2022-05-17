5f4f97472881b3b7a71dc553202bde4a01b2a03b
Higor <higorpiresoliveira@gmail.com>
2022-05-17 08:50:13 -0300

    ensure HttpLoadContentChildren throws UnexpectedError if HttpClient returns 404

--- test/data/usecases/http_load_content_children_test.dart
+++ test/data/usecases/http_load_content_children_test.dart
@@ -64,6 +64,10 @@ void main() {
     mockRequest().thenAnswer((_) async => data);
   }
 
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     url = faker.internet.httpUrl();
     httpClient = MockHttpClient();
@@ -174,6 +178,14 @@ void main() {
     mockHttpData([
       {'invalid_key': 'invalid_data'}
     ]);
+
+    final future = sut.loadContentChildren(faker.guid.guid());
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
 
     final future = sut.loadContentChildren(faker.guid.guid());
 
