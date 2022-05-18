d22a43b9f6d56cba56a1652a2259f820e4333026
Higor <higorpiresoliveira@gmail.com>
2022-05-18 13:33:31 -0300

    improving code style

--- lib/ui/pages/content/content_page.dart
+++ lib/ui/pages/content/content_page.dart
@@ -113,7 +113,11 @@ class ContentPage extends StatelessWidget {
                     }
 
                     if (snapshots.item4.hasError) {
-                      return Text(snapshots.item4.error.toString(), style: const TextStyle(fontSize: 16), textAlign: TextAlign.center);
+                      return Text(
+                        snapshots.item4.error.toString(),
+                        style: const TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+                      );
                     }
 
                     if (!snapshots.item4.hasData || snapshots.item4.data!.isEmpty) {
